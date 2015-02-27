d72a37029cfd16536446169d26cf27eb98e72234
willy-ahva <willy.mesnage@akeneo.com>
2015-02-27 17:48:01 +0100

    MC-208: Add PurgerInterface and DeltaTablePurger

---
+++ Command/PurgeDeltaCommand.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace Pim\Bundle\MagentoConnectorBundle\Command;
+
+use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+/**
+ * Purge delta tables for a given job instance code
+ *
+ * @author    Willy Mesnage <willy.mesnage@akeneo.com>
+ * @copyright 2015 Akeneo SAS (http://www.akeneo.com)
+ * @license   http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ */
+class PurgeDeltaCommand extends ContainerAwareCommand
+{
+    /**
+     * {@inheritdoc}
+     */
+    protected function configure()
+    {
+        $this
+            ->setName('magento-connector:delta:purge')
+            ->setDescription('Purges delta table from database for a given job instance code.')
+            ->addArgument(
+                'job_instance_code',
+                InputArgument::REQUIRED,
+                'From which job instance want you remove ?'
+            );
+    }
+
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $deltaTablePurger = $this->getDeltaTablePurger();
+        $jobInstanceCode  = $input->getArgument('job_instance_code');
+
+        $output->writeln(sprintf('<info>Executing command for "%s" job instance.<info>', $jobInstanceCode));
+
+        try {
+            $deltaTablePurger->purge($jobInstanceCode);
+            $output->writeln(
+                sprintf('<info>Delta related to "%s" job instance has been purge.<info>', $jobInstanceCode)
+            );
+        } catch (\Exception $e) {
+            $output->writeln(
+                sprintf('<error>Error appears for "%s" job code : "%s"<error>', $jobInstanceCode, $e->getMessage())
+            );
+
+            return 1;
+        }
+
+        return 0;
+    }
+
+    /**
+     * Get the mapping purger
+     *
+     * @return \Pim\Bundle\MagentoConnectorBundle\Purger\DeltaTablePurger
+     */
+    protected function getDeltaTablePurger()
+    {
+        return $this->getContainer()->get('pim_magento_connector.purger.delta');
+    }
+}
