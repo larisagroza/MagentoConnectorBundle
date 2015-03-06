53b51fa1a5918ef5ba7e090ee8d732f6fddb0675
willy-ahva <willy.mesnage@akeneo.com>
2015-03-06 13:51:38 +0100

    MC-224: Add a new DeltaProductAssociationReader

--- Reader/MongoDBODM/DeltaProductAssociationReader.php
+++ Reader/MongoDBODM/DeltaProductAssociationReader.php
@@ -12,25 +12,25 @@ use Pim\Bundle\CatalogBundle\Repository\ProductRepositoryInterface;
 use Pim\Bundle\TransformBundle\Converter\MetricConverter;
 
 /**
- * Delta product reader for MongoDB
+ * Delta product association reader for MongoDB
  *
- * @author    Romain Monceau <romain@akeneo.com>
+ * @author    Willy Mesnage <willy.mesnage@akeneo.com>
  * @copyright 2015 Akeneo SAS (http://www.akeneo.com)
  * @license   http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
  */
-class DeltaProductReader extends ODMProductReader
+class DeltaProductAssociationReader extends ODMProductReader
 {
     /** @var EntityRepository */
     protected $deltaRepository;
 
     /**
-     * @param ProductRepositoryInterface $repository
-     * @param ChannelManager             $channelManager
-     * @param CompletenessManager        $completenessManager
-     * @param MetricConverter            $metricConverter
-     * @param DocumentManager            $documentManager
-     * @param boolean                    $missingCompleteness
-     * @param EntityRepository           $deltaRepository
+     * @param ProductRepositoryInterface     $repository
+     * @param ChannelManager                 $channelManager
+     * @param CompletenessManager            $completenessManager
+     * @param MetricConverter                $metricConverter
+     * @param DocumentManager                $documentManager
+     * @param boolean                        $missingCompleteness
+     * @param EntityRepository               $deltaRepository
      */
     public function __construct(
         ProductRepositoryInterface $repository,
