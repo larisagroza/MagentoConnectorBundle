1515bd8b72b75bfb15411feffcf9d0979e22a062
willy-ahva <willy.mesnage@akeneo.com>
2015-03-10 15:07:36 +0100

    MC-221: Improve mapping setter from SimpleMappingManager

--- Manager/SimpleMappingManager.php
+++ Manager/SimpleMappingManager.php
@@ -22,6 +22,9 @@ class SimpleMappingManager
     /** @var string */
     protected $className;
 
+    /** @var array */
+    protected $savedMapping = [];
+
     /**
      * Constructor.
      *
@@ -54,30 +57,59 @@ class SimpleMappingManager
      */
     public function setMapping(array $mapping, $identifier)
     {
-        $this->pruneOldMapping($identifier);
+        if (!$this->isMappingSaved($mapping, $identifier)) {
+            $this->pruneOldMapping($identifier);
 
-        foreach ($mapping as $mappingItem) {
-            if ($mappingItem['source'] != '') {
-                $simpleMappingItem = $this->getEntityRepository()->findOneBy(
-                    array(
-                        'identifier' => $identifier,
-                        'source'     => $mappingItem['source'],
-                    )
-                );
-
-                if (!$simpleMappingItem) {
+            foreach ($mapping as $mappingItem) {
+                if ($mappingItem['source'] != '') {
                     $simpleMappingItem = new SimpleMapping();
                     $simpleMappingItem->setIdentifier($identifier);
                     $simpleMappingItem->setSource($mappingItem['source']);
+                    $simpleMappingItem->setTarget($mappingItem['target']);
+                    $this->objectManager->persist($simpleMappingItem);
                 }
-
-                $simpleMappingItem->setTarget($mappingItem['target']);
-
-                $this->objectManager->persist($simpleMappingItem);
             }
-        }
 
-        $this->objectManager->flush();
+            $this->objectManager->flush();
+            $this->updateSavedMapping($mapping, $identifier);
+        }
+    }
+
+    /**
+     * Update saved mapping
+     *
+     * @param array  $mapping
+     * @param string $identifier
+     */
+    protected function updateSavedMapping(array $mapping, $identifier)
+    {
+        $this->savedMapping[] = $this->getKey($mapping, $identifier);
+    }
+
+    /**
+     * Checks if given mapping is saved
+     *
+     * @param array  $mapping
+     * @param string $identifier
+     *
+     * @return bool
+     */
+    protected function isMappingSaved(array $mapping, $identifier)
+    {
+        return in_array($this->getKey($mapping, $identifier), $this->savedMapping);
+    }
+
+    /**
+     * Returns key from mapping and identifier
+     *
+     * @param array  $mapping
+     * @param string $identifier
+     *
+     * @return string
+     */
+    protected function getKey(array $mapping, $identifier)
+    {
+        return md5(sprintf('%s%s', json_encode($mapping), $identifier));
     }
 
     /**
